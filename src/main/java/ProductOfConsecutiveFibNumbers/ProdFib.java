8a3e4d05bbc791261dde121ede0a20c10c605bac
romanstetsiuk1 <romansteciuk021@gmail.com>
2019-04-26 11:58:43 +0200

    Consecutive Fib Number

---
+++ src/main/java/ProductOfConsecutiveFibNumbers/ProdFib.java
@@ -0,0 +1,71 @@
+package ProductOfConsecutiveFibNumbers;
+
+/*
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(m) verifying F(m) * F(m+1) = prodyou will return
+
+[F(m), F(m+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(m) being the smallest one such as F(m) * F(m+1) > prod.
+
+Examples
+productFib(714) # should return {21, 34, 1},
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return {34, 55, 0},
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+ */
+
+public class ProdFib {
+
+    public static long[] productFib(long prod) {
+
+        long x0 = 0;
+        long x1 = 1;
+        long x2;
+
+        while ((x0 + x1) > 0) {
+            x2 = x0 + x1;
+            if ((x0 * x1) == prod) {
+                return new long[]{x0, x1, 1};
+            }
+            if ((x0 * x1) > prod) {
+                return new long[]{x0, x1, 0};
+            }
+            x0 = x1;
+            x1 = x2;
+        }
+
+        return null;
+    }
+}
+
+// Another way
+
+/*
+  public static long[] productFib(long prod) {
+    long a = 0L;
+    long b = 1L;
+    while (a * b < prod) {
+      long tmp = a;
+      a = b;
+      b = tmp + b;
+    }
+    return new long[] { a, b, a * b == prod ? 1 : 0 };
+   }
+ */

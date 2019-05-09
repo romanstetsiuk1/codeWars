cee4a3747cc4c0b5181019b53602a1dc546d8ad8
romanstetsiuk1 <romansteciuk021@gmail.com>
2019-05-09 13:38:41 +0200

    Create class Dubstep, by convert String

---
+++ src/test/java/Dubstep/DubstepTest.java
@@ -0,0 +1,19 @@
+package Dubstep;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DubstepTest {
+
+    @Test
+    public void Test1() {
+        assertEquals("ABC", new Dubstep().SongDecoder("WUBWUBABCWUB"));
+    }
+
+    @Test
+    public void Test2() {
+        assertEquals("R L", new Dubstep().SongDecoder("RWUBWUBWUBLWUB"));
+    }
+
+}
